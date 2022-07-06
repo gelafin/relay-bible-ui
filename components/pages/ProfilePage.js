d7f2fd2f12609482b9af95064bf821876cb514bb
gelafin <themarkymarkers@gmail.com>
2022-07-05 17:54:10 -0700

    adjust indent to 2 spaces

--- components/pages/ProfilePage.js
+++ components/pages/ProfilePage.js
@@ -2,16 +2,16 @@ import { React } from 'react';
 import { Text } from 'react-native';
 
 const ProfilePage = ({ yourNumbers }) => {
-    return (
-        <div>
-            <Text>hi</Text>
-            {
-                yourNumbers.map(currentNumber => (
-                    <Text key={currentNumber}>{currentNumber}</Text>
-                ))
-            }
-        </div>
-    );
+  return (
+    <div>
+      <Text>hi</Text>
+      {
+        yourNumbers.map(currentNumber => (
+          <Text key={currentNumber}>{currentNumber}</Text>
+        ))
+      }
+    </div>
+  );
 }
 
 export default ProfilePage;
