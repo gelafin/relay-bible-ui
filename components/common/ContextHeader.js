06fd24ee8b43faae2c826982b03d307bab7ff44e
gelafin <themarkymarkers@gmail.com>
2022-07-12 18:49:26 -0700

    abey stack nav

--- components/common/ContextHeader.js
+++ components/common/ContextHeader.js
@@ -5,12 +5,8 @@ import { Appbar } from 'react-native-paper';
 import { layoutStyles } from '../../assets/stylesheets/layouts';
 
 const ContextHeader = ({headingText}) => {
-  // TODO: implement navigation
-  const goBack = () => console.log('back button pressed');
-
   return (
     <Appbar.Header style={[layoutStyles.appBar, layoutStyles.appBarSmall]}>
-      <Appbar.BackAction onPress={goBack} />
       <Appbar.Content title={headingText} titleStyle={layoutStyles.appBarSmallText} />
     </Appbar.Header>
   );
