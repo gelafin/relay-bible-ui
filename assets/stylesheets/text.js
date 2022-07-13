11bb9f0ccc5c7c20cad751d2a846c464796d00d0
gelafin <themarkymarkers@gmail.com>
2022-07-12 19:49:17 -0700

    style reading page and use realistic placeholder data

---
+++ assets/stylesheets/text.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const textStyles = StyleSheet.create({
+  superscript: {
+    fontSize: '0.7rem'
+  }
+});
+
+export { textStyles };
--- components/pages/ReadingPage.js
+++ components/pages/ReadingPage.js
@@ -1,27 +1,44 @@
-import { React } from 'react';
+// remove Text newline (referenced Jul 2022): https://stackoverflow.com/a/70289492/14257952
+
+import { React, useState } from 'react';
 import { Text } from 'react-native';
 
 // custom
 import { PageStyler } from './PageStyler.js';
 import { ContextHeader } from '../common/ContextHeader.js';
 import { layoutStyles } from '../../assets/stylesheets/layouts.js';
+import { textStyles } from '../../assets/stylesheets/text.js';
 
-const sampleText = `
-    The Creation of the World
-    1 In the abeginning, God created the heavens and the earth. 2 The earth was bwithout form and void, and darkness was over the face of the deep. And the Spirit of God was hovering over the face of the waters.
-
-    3 And God said, c“Let there be light,” and there was light. 4 And God saw that the light was good. And God separated the light from the darkness. 5 God called the light Day, and the darkness he called Night. And there was evening and there was morning, the first day.
-
-    6 And God said, d“Let there be an expanse1 in the midst of the waters, and let it separate the waters from the waters.” 7 And God made2 the expanse and eseparated the waters that were under the expanse from the waters that were fabove the expanse. And it was so. 8 And God called the expanse Heaven.3 And there was evening and there was morning, the second day.
-
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis in leo sit amet aliquet. Praesent ullamcorper cursus nunc id rutrum. Vestibulum sit amet nulla sem. Proin turpis quam, dignissim vitae nulla eu, tincidunt fermentum justo. Curabitur sed fermentum tellus. Duis ac massa semper ligula pellentesque mattis eu sit amet dolor. Donec eu dui nisi. Mauris non mauris ut nulla maximus convallis eu eu urna. Phasellus congue`;
+// example for API route GET /verses/:bookName/:chapterNumber
+// regular 0-indexed array of one string per verse (no spaces is slightly preferred, but whatever is easier with sample data)
+const sampleVerses = [
+  'In the beginning, God created the heavens and the earth.',
+  'The earth was without form and void, and darkness was over the face of the deep. And the Spirit of God was hovering over the face of the waters.',
+  'And God said, “Let there be light,” and there was light.',
+  'And God saw that the light was good. And God separated the light from the darkness.',
+  'God called the light Day, and the darkness he called Night. And there was evening and there was morning, the first day.',
+  'And God said, “Let there be an expanse in the midst of the waters, and let it separate the waters from the waters.” And God made the expanse and eseparated the waters that were under the expanse from the waters that were fabove the expanse. And it was so.',
+  'And God called the expanse Heaven. And there was evening and there was morning, the second day.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis in leo sit amet aliquet. Praesent ullamcorper cursus nunc id rutrum. Vestibulum sit amet nulla sem. Proin turpis quam, dignissim vitae nulla eu, tincidunt fermentum justo. Curabitur sed fermentum tellus. Duis ac massa semper ligula pellentesque mattis eu sit amet dolor. Donec eu dui nisi. Mauris non mauris ut nulla maximus convallis eu eu urna. Phasellus congue'
+];
 
 const ReadingPage = () => {
+  const [currentBook, setCurrentBook] = useState('Genesis');
+  const [currentChapter, setCurrentChapter] = useState(1);
+
   return (
     <>
-      <ContextHeader headingText="Top o' the reading page to you, laddies"></ContextHeader>
+      <ContextHeader headingText={`${currentBook} ${currentChapter}`}></ContextHeader>
       <PageStyler customPageStyle={layoutStyles.readingPage}>
-        <Text>{sampleText}</Text>
+        <Text>
+          {sampleVerses.map((verseText, index) =>
+            <Text key={'v' + index}>
+              {/* verse number */}
+              <Text style={textStyles.superscript}>{index + 1}</Text>
+              <Text>{' ' + verseText + ' '}</Text>
+            </Text>
+          )}
+        </Text>
       </PageStyler>
     </>
   );
