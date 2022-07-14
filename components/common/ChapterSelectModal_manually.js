0fdfb2ee28bf25a8cfc471ca7a114d6359b325d7
gelafin <themarkymarkers@gmail.com>
2022-07-14 13:59:08 -0700

    enable scroll in chapter select modal

--- components/common/ChapterSelectModal_manually.js
+++ components/common/ChapterSelectModal_manually.js
@@ -1,13 +1,13 @@
 import { React, useState } from 'react';
 import { Modal, Portal } from 'react-native-paper';
-import { FlatList, Text, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 // custom
 import { CHAPTER_COUNTS } from '../../constants/chapterCounts';
 
 const containerStyle = {backgroundColor: 'white', padding: 20, width: '80vw', marginLeft: 'auto', marginRight: 'auto', height: '100%'};
 
-const ChapterSelectModal = ({visible, setVisible, onChapterSelect}) => {
+const ChapterSelectModal_manually = ({visible, setVisible, onChapterSelect}) => {
   const [selectedBook, setSelectedBook] = useState();
   const [selectedChapter, setSelectedChapter] = useState();
 
@@ -19,20 +19,9 @@ const ChapterSelectModal = ({visible, setVisible, onChapterSelect}) => {
   };
 
   // process data into array of objects, expected by FlatList
-  const booksData = Object.keys(CHAPTER_COUNTS).map((key) => (
-    {bookName: key, chapterCount: CHAPTER_COUNTS[key], }
-  ));
+  const bookNames = Object.keys(CHAPTER_COUNTS);
 
-  console.log(booksData);
-
-  const renderBookNameItem = ({item}) => (
-    <TouchableOpacity
-      style={{padding: 20, cursor: 'pointer'}}
-      onPress={() => {console.log('selected ', item.bookName); setSelectedBook(item.bookName)}}
-    >
-      <Text style={{fontSize: 32}}>{item.bookName}</Text>
-    </TouchableOpacity>
-  );
+  console.log(bookNames);
 
   return (
     <>
@@ -41,11 +30,20 @@ const ChapterSelectModal = ({visible, setVisible, onChapterSelect}) => {
           <Text onPress={handleSelection}>Click this to test. The below do not work yet</Text>
           <Text>Wat u want, Genesis 1 or Genesis 1? Or we can do Geonosis pew pew</Text>
           {/* list of books */}
-          <FlatList data={booksData} renderItem={renderBookNameItem} keyExtractor={item => item.bookName} />
+          <ScrollView style={{height: '100%'}}>
+            {
+              bookNames.map((bookName) => (
+                // TODO: View should be TouchableOpacity
+                <View key={bookName} onPress={() => {setSelectedBook(bookName)}}>
+                  <Text>{bookName}</Text>
+                </View>
+              ))
+            }
+          </ScrollView>
         </Modal>
       </Portal>
     </>
   );
 };
 
-export { ChapterSelectModal };
+export { ChapterSelectModal_manually };
